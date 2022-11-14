7df0b6e7ec3fd278c8f01954521e96cec70dfc03
AelanderFarias <aelanderf@gmail.com>
2022-11-13 23:16:09 -0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Destino from "./Pages";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Destino />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    
     alignItems: 'center',
     justifyContent: 'center',
   },
